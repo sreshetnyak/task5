6ae1d1aeb8a43fdeeaaf13d6d1627c94cac453c8
sreshetnyak <reshetniak.sergey@gmail.com>
2014-01-13 13:14:30 +0200

    Initial commit from local git

---
+++ array/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  array
+//
+//  Created by sergey on 1/13/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ array/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  array
+//
+//  Created by sergey on 1/13/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
